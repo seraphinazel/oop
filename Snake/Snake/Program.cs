dad26c6352ce7b92cb248a2a9d6c716c7b0facab
Suarra <seraphin.azel@gmail.com>
2018-08-26 20:32:44 +0700

    9

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -22,7 +22,8 @@ namespace Snake
             rightLine.Drow();
 
             Point p = new Point(4, 5, '*');
-            p.Draw();
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
 
             Console.ReadLine();
         }
